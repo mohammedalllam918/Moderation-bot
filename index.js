2d3e2f39390991d81d6ae7662779a06793254422
mohammedalllam918 <71492055+mohammedalllam918@users.noreply.github.com>
2021-06-06 10:02:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ client.on('messageUpdate', (o, message) => {
 require('./utils/handlers/editHandles')(client, message);
 })
 
-client.login(token).catch(() => {
+client.login(Nzk1MDU0MDkyNDg1NTI1NTA0.X_Dx0w.zM3d2pe8idsQG3gjeVjYPUGHLwU).catch(() => {
     console.log('Incorrect token given!! Please make sure to go to the config folder and check all files and fill them out')
 });
